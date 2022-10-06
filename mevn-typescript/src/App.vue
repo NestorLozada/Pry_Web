d8b4f88e2d39bd4b4c8b8a7cb5bd4d94180984d5
NestorLozada <martinlozada1@gmail.com>
2022-10-06 02:32:37 -0500

    AddingNavbar

--- mevn-typescript/src/App.vue
+++ mevn-typescript/src/App.vue
@@ -1,15 +1,17 @@
 <template>
+  <NavbarVue/>
   <router-view/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import NavbarVue from './components/Navbar.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    
-  }
+    NavbarVue
+  },
 });
 </script>
 
---
+++ mevn-typescript/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/">Tareas</router-link>
+
+            </li>
+            <li>
+                <router-link to="/task/new">Crear tarea</router-link>        
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from '@vue/runtime-core';
+    
+        export default defineComponent({
+            name: "NavbarVue",
+        })
+</script> 
