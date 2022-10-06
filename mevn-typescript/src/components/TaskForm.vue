666e68ab15c98d1bbbd149af493059890b4e927e
NestorLozada <martinlozada1@gmail.com>
2022-10-05 21:32:59 -0500

    taskformConfig

--- mevn-typescript/src/components/TaskForm.vue
+++ mevn-typescript/src/components/TaskForm.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1> Formulario de Tareas</h1>
+    <for>
+        <input type="text" placeholder="Incerte texto" v-model="task.title">
+        <textarea  rows="3" placeholder="Incerte texto" v-model="task.description"></textarea>
+        <button> Guardar </button>
+    </for>
 </template>
+
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {Task} from '../interfaces/Task';
+
+export default defineComponent({
+    data(){
+        return{
+            task: {  
+
+            } as Task
+        };
+    },
+});
+
+</script>
